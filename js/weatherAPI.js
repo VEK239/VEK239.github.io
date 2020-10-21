cd3f72be543c823222c1d98c83adb4a2bb21bfe9
Elizaveta Vlasova <vlasova.elizaveta@yandex.ru>
2020-10-21 12:56:43 +0300

    Trying to fix api request

--- js/weatherAPI.js
+++ js/weatherAPI.js
@@ -4,18 +4,18 @@ class WeatherAPI {
     }
 
     async getByCityName(cityName) {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.apiKey}&units=metric`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.apiKey}&units=metric`);
         return await response.json()
     }
 
     async getByCityCoordinates(coordinates) {
         const [lat, lon] = [coordinates.coords.latitude, coordinates.coords.longitude];
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=metric`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=metric`);
         return await response.json()
     }
 
     getIconURL(iconCode) {
-        return `http://openweathermap.org/img/wn/${iconCode}.png`
+        return `https://openweathermap.org/img/wn/${iconCode}.png`
     }
 }
 
